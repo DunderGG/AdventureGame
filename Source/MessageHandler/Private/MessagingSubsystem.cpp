0310ca7ac498f7a5431aae5f2076586ae8d8b9cd
David Bennehag <david.bennehag@gmail.com>
2026-03-13 09:36:00 +0100

    Turned MessageManager into GameInstanceSubsystem

--- Source/MessageHandler/Private/MessagingSubsystem.cpp
+++ Source/MessageHandler/Private/MessagingSubsystem.cpp
@@ -1,38 +1,53 @@
 // Copyright (C) 2026 dunder.gg [GNU GPLv3]
 
-
 #include "MessagingSubsystem.h"
+#include "Subsystems/GameInstanceSubsystem.h"
 
 /*
 * These are basically passthrough functions
 *	When the time changes, we want to trigger the delegate, which will notify all subscribers that the time has changed.
 */
-void UMessagingSubsystem::updateTimeOfDay(FTimeData newTime)
+void UMessagingSubsystem::updateTimeOfDay(const FTimeData& newTime)
 {
 	onTimeChange.Broadcast(newTime);
 }
 
-void UMessagingSubsystem::updateHourOfDay(int newHour)
+void UMessagingSubsystem::updateHourOfDay(const int newHour)
 {
 	onHourChange.Broadcast(newHour);
 }
 
-void UMessagingSubsystem::updateDayOfYear(int newDayOfYear)
+void UMessagingSubsystem::updateDayOfYear(const int newDayOfYear)
 {
 	onDayOfYearChange.Broadcast(newDayOfYear);
 }
 
-void UMessagingSubsystem::updateMonth(int newMonth)
+void UMessagingSubsystem::updateMonth(const int newMonth)
 {
 	onMonthChange.Broadcast(newMonth);
 }
 
-void UMessagingSubsystem::updateYear(int newYear)
+void UMessagingSubsystem::updateYear(const int newYear)
 {
 	onYearChange.Broadcast(newYear);
 }
 
-void UMessagingSubsystem::updateTemperature(float newTemperature)
+void UMessagingSubsystem::updateTemperature(const float newTemperature)
 {
 	onTemperatureChange.Broadcast(newTemperature);
+}
+
+UMessagingSubsystem* UMessagingSubsystem::Get(const UObject* worldContextObject)
+{
+	UWorld* world = GEngine->GetWorldFromContextObject(worldContextObject, 
+													   EGetWorldErrorMode::LogAndReturnNull);
+	if (world)
+	{
+		UGameInstance* gameInstance = world->GetGameInstance();
+		if (gameInstance)
+		{
+			return gameInstance->GetSubsystem<UMessagingSubsystem>();
+		}
+	}
+	return nullptr;
 }
--- Source/MessageHandler/Public/MessagingSubsystem.h
+++ Source/MessageHandler/Public/MessagingSubsystem.h
@@ -3,27 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Subsystems/WorldSubsystem.h"
 #include "FTimeData.h"
 #include "MessagingSubsystem.generated.h"
+
+class UGameInstance;
 
 /*
 * A delegate is kind of like a mailing list,
 * where you can subscribe to it and then when the event happens,
 * all the subscribers will be notified.
 */
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTimeChangeDelegate, FTimeData, timeData);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FHourChangeDelegate, int, hour);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDayOfYearChangeDelegate, int, dayOfYear);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMonthChangeDelegate, int, month);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FYearChangeDelegate, int, year);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTemperatureChangeDelegate, float, currentTemp);
-
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPreciseTimeChangeDelegate, float, preciseTime);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTimeChangeDelegate, const FTimeData&, timeData);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FHourChangeDelegate, const int, hour);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDayOfYearChangeDelegate, const int, dayOfYear);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMonthChangeDelegate, const int, month);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FYearChangeDelegate, const int, year);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTemperatureChangeDelegate, const float, currentTemp);
 /**
  * 
  */
 UCLASS()
-class MESSAGEHANDLER_API UMessagingSubsystem : public UWorldSubsystem
+class MESSAGEHANDLER_API UMessagingSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 	
@@ -33,6 +34,9 @@ protected:
 
 public:
 #pragma region DELEGATES
+	UPROPERTY(BlueprintAssignable, Category = "MessageManager | Time")
+	FPreciseTimeChangeDelegate onPreciseTimeChange;
+
 	UPROPERTY(BlueprintAssignable, Category = "MessageManager | Time")
 	FTimeChangeDelegate onTimeChange;
 
@@ -53,11 +57,14 @@ public:
 #pragma endregion
 
 #pragma region TRIGGERS
-	void updateTimeOfDay(FTimeData newTime);
-	void updateHourOfDay(int newHour);
-	void updateDayOfYear(int newDayOfYear);
-	void updateMonth(int newMonth);
-	void updateYear(int newYear);
-	void updateTemperature(float newTemperature);
+	void updatePreciseTime(float preciseTime);
+	void updateTimeOfDay(const FTimeData& newTime);
+	void updateHourOfDay(const int newHour);
+	void updateDayOfYear(const int newDayOfYear);
+	void updateMonth(const int newMonth);
+	void updateYear(const int newYear);
+	void updateTemperature(const float newTemperature);
 #pragma endregion
+
+	static UMessagingSubsystem* Get(const UObject* worldContextObject);
 };
